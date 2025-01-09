7ea0b3b7a28b83f39e77fd1e867accd937fb0b54
Mateus Alves <57416137+alvesmateus63@users.noreply.github.com>
2025-01-08 21:47:21 -0300

    ajustes

--- include/main.cpp
+++ include/main.cpp
@@ -11,72 +11,6 @@ void imprimir_instrucao() {
     cout << "  main.out -c -l <arquivo_desc> <arquivo_saida>  # Criar grafo aleatório com lista" << endl;
 }
 
-int main() {
-    // Criando um grafo com 4 vértices, não direcionado e sem ponderação
-    //GrafoLista grafo(4, false, false, false);
-    GrafoMatriz grafo(5, false, false, false);
-    grafo.novo_grafo("config.txt");
-    grafo.imprimir_grafo("grafo_impressao.txt");
-    // Adicionando vértices e arestas
-    //grafo.adicionar_vertice(0);
-    //grafo.adicionar_vertice(1);
-    //grafo.adicionar_vertice(2);
-    //grafo.adicionar_vertice(3);
-
-    // Adicionando arestas
-    //grafo.adicionar_aresta(0, 1); // Aresta entre 0 e 1
-    //grafo.adicionar_aresta(1, 2); // Aresta entre 1 e 2
-    //grafo.adicionar_aresta(2, 3); // Aresta entre 2 e 3
-    //grafo.adicionar_aresta(3, 0); // Aresta entre 3 e 0
-
-    // Testando funções
-    // std::cout << "Número de componentes conexos: " << grafo.n_conexo() << std::endl;
-    // std::cout << "Grau do vértice 0: " << grafo.get_grau(0) << std::endl;
-    // std::cout << "O grafo é bipartido? " << (grafo.eh_bipartido() ? "Sim" : "Não") << std::endl;
-    // std::cout << "O grafo é completo? " << (grafo.eh_completo() ? "Sim" : "Não") << std::endl;
-    // std::cout << "O grafo é uma árvore? " << (grafo.eh_arvore() ? "Sim" : "Não") << std::endl;
-    // std::cout << "O grafo possui articulação? " << (grafo.possui_articulacao() ? "Sim" : "Não") << std::endl;
-    // std::cout << "O grafo possui ponte? " << (grafo.possui_ponte() ? "Sim" : "Não") << std::endl;
-    // std::cout << "Número de componentes conexos: " << grafo.n_conexo() << std::endl;
-
-    // std::cout << "O grafo é bipartido? " << (grafo.eh_bipartido() ? "Sim" : "Não") << std::endl;
-    // std::cout << "O grafo é completo? " << (grafo.eh_completo() ? "Sim" : "Não") << std::endl;
-    // std::cout << "O grafo é uma árvore? " << (grafo.eh_arvore() ? "Sim" : "Não") << std::endl;
-    // std::cout << "O grafo possui articulação? " << (grafo.possui_articulacao() ? "Sim" : "Não") << std::endl;
-    // std::cout << "O grafo possui ponte? " << (grafo.possui_ponte() ? "Sim" : "Não") << std::endl;
-    // Imprimindo o grafo (caso você tenha implementado imprimir_grafo)
-    //grafo.imprimir_grafo("grafo_impressao.txt");
-
-    //GrafoMatriz grafo(4, false, false, false); // Inicializa o grafo com 4 vértices
-    //grafo.carregar_grafo("grafo_teste.txt");         // Carrega o grafo a partir do arquivo
-    //grafo.imprimir_grafo("grafo_matriz_teste.txt"); // Imprime o grafo para verificar
-
-    // Adicionando vértices e arestas
-    //grafo.adicionar_vertice(0);
-    //grafo.adicionar_vertice(1);
-    //grafo.adicionar_vertice(2);
-    //grafo.adicionar_vertice(3);
-
-    // Adicionando arestas
-    //grafo.adicionar_aresta(0, 1); // Aresta entre 0 e 1
-    //grafo.adicionar_aresta(1, 2); // Aresta entre 1 e 2
-    //grafo.adicionar_aresta(2, 3); // Aresta entre 2 e 3
-    //grafo.adicionar_aresta(3, 0); // Aresta entre 3 e 0
-
-    // Testando funções
-    //std::cout << "Número de componentes conexos: " << grafo.n_conexo() << std::endl;
-    //std::cout << "Grau do vértice 0: " << grafo.get_grau(0) << std::endl;
-    //std::cout << "O grafo é bipartido? " << (grafo.eh_bipartido() ? "Sim" : "Não") << std::endl;
-    //std::cout << "O grafo é completo? " << (grafo.eh_completo() ? "Sim" : "Não") << std::endl;
-    //std::cout << "O grafo é uma árvore? " << (grafo.eh_arvore() ? "Sim" : "Não") << std::endl;
-    //std::cout << "O grafo possui articulação? " << (grafo.possui_articulacao() ? "Sim" : "Não") << std::endl;
-    //std::cout << "O grafo possui ponte? " << (grafo.possui_ponte() ? "Sim" : "Não") << std::endl;
-
-    // Imprimindo o grafo (caso você tenha implementado imprimir_grafo)
-    //grafo.imprimir_grafo("grafo_impressao.txt");
-
-}
-
 int main(int argc, char* argv[]) {
 
     if(argc < 3) {
@@ -152,9 +86,15 @@ int main(int argc, char* argv[]) {
         string arquivo_saida = argv[4];
 
         if(estrutura == "-m") {
-            // Matriz aleatório
+            GrafoMatriz grafo(4, false, false, false);
+
+            grafo.novo_grafo("config.txt");
+            grafo.imprimir_grafo("grafo_impressao.txt");
         } else if(estrutura == "-l") {
-            // Lista aleatória
+            GrafoLista grafo(4, false, false, false);
+
+            grafo.novo_grafo("config.txt");
+            grafo.imprimir_grafo("grafo_impressao.txt");
         } else {
             imprimir_instrucao();
             return 1;
